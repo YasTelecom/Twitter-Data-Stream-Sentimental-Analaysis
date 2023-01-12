c9e1b19ac68c7c74dd0acd53da69981ae343c676
annieliu75 <81642357+annieliu75@users.noreply.github.com>
2023-01-12 17:01:58 +0100

    fichier pour récupérer les tweets

---
+++ ingest-tweets.py
@@ -0,0 +1,42 @@
+import tweepy
+import json
+from kafka import KafkaProducer
+
+# Configure the authentication keys for the Twitter API
+bearer_token = "AAAAAAAAAAAAAAAAAAAAAMxXkAEAAAAAFC3teGdOH64EKRkhCiWANfWxwvk%3DvPTipAtXE0gr2YHUcBTp0MQ8bYz65LAbRyiiGLFIbCTvw4FZiD"
+client = tweepy.Client(bearer_token=bearer_token)
+
+
+# Parameters
+nMaxTweet = 20
+keyword = 'ecology' 
+query = keyword + ' -is:retweet'
+topic_name = 'raw-tweets'
+
+# Producer
+producer = KafkaProducer(bootstrap_servers="localhost:9092")
+
+# Paginator
+paginator = tweepy.Paginator(
+    client.search_recent_tweets, 
+    query = query, 
+    tweet_fields = ['lang'], 
+    max_results = 500
+    )
+
+# add tweet to topic in a json
+for tweet in paginator.flatten(limit=nMaxTweet):
+
+    content = str(tweet.text)
+    lang = str(tweet.lang)
+
+    tweet_dict = {
+        "content": content,
+        "lang": lang,
+    }
+    
+    tweet = json.dumps(tweet_dict).encode('utf-8')
+    if tweet['lang'] == 'en':
+        producer.send(topic_name, tweet)
+    # print("Sending message {} to topic: {}".format(tweet, topic_name)) 
+
