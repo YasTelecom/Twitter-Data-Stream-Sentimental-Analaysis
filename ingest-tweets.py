f3a3c5583c2afebda7584630f9addc5d78c0d47f
potato <81642357+annieliu75@users.noreply.github.com>
2023-01-15 19:29:19 +0100

    change ingest to have continuous data

--- ingest-tweets.py
+++ ingest-tweets.py
@@ -1,42 +1,46 @@
 import tweepy
 import json
-from kafka import KafkaProducer
+import time
+# from kafka import KafkaProducer
 
 # Configure the authentication keys for the Twitter API
-bearer_token = "AAAAAAAAAAAAAAAAAAAAAMxXkAEAAAAAFC3teGdOH64EKRkhCiWANfWxwvk%3DvPTipAtXE0gr2YHUcBTp0MQ8bYz65LAbRyiiGLFIbCTvw4FZiD"
+bearer_token="AAAAAAAAAAAAAAAAAAAAAMxXkAEAAAAAFC3teGdOH64EKRkhCiWANfWxwvk%3DvPTipAtXE0gr2YHUcBTp0MQ8bYz65LAbRyiiGLFIbCTvw4FZiD"
 client = tweepy.Client(bearer_token=bearer_token)
 
 
 # Parameters
-nMaxTweet = 20
+nMaxTweet = 100
 keyword = 'ecology' 
 query = keyword + ' -is:retweet'
-topic_name = 'raw-tweets'
+topic_name = 'ecology-tweets'
 
 # Producer
-producer = KafkaProducer(bootstrap_servers="localhost:9092")
+# producer = KafkaProducer(bootstrap_servers="localhost:9092")
 
 # Paginator
 paginator = tweepy.Paginator(
     client.search_recent_tweets, 
-    query = query, 
-    tweet_fields = ['lang'], 
-    max_results = 500
+    query=query, 
+    tweet_fields=['lang'], 
+    max_results=100
     )
 
 # add tweet to topic in a json
-for tweet in paginator.flatten(limit=nMaxTweet):
-
-    content = str(tweet.text)
-    lang = str(tweet.lang)
-
-    tweet_dict = {
-        "content": content,
-        "lang": lang,
-    }
+while True :
+    for tweet in paginator.flatten(limit=nMaxTweet):
     
-    tweet = json.dumps(tweet_dict).encode('utf-8')
-    if tweet['lang'] == 'en':
-        producer.send(topic_name, tweet)
-    # print("Sending message {} to topic: {}".format(tweet, topic_name)) 
+        content = str(tweet.text)
+        lang = str(tweet.lang)
 
+        tweet_dict = {
+            "content": content,
+            "lang": lang,
+        }
+        if tweet_dict["lang"] == 'en':
+            print(tweet_dict["lang"])
+        tweet = json.dumps(tweet_dict).encode('utf-8')
+        # producer.send(topic_name, tweet)
+        # print("Sending message {} to topic: {}".format(tweet, topic_name)) 
+
+    nMaxTweet = 5
+    time.sleep(1)
